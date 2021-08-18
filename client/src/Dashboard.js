20852e56a53420733c5a6e5c99549dc02ee00b9a
haptork <hap_tork@hotmail.com>
2021-08-18 18:07:26 +0530

    Added dashboard

---
+++ client/src/Dashboard.js
@@ -0,0 +1,217 @@
+import React from "react";
+import withStyles from "@material-ui/core/styles/withStyles";
+//import PropTypes from "prop-types";
+// core components
+import Grid from "@material-ui/core/Grid";
+import GridItem from "./components/Grid/GridItem.js";
+import Footer from "./components/Footer/Footer.js";
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import classNames from 'classnames';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Accordion from '@material-ui/core/Accordion';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import dashboardStyle from "./assets/jss/material-dashboard-react/views/dashboardStyle.js";
+// charts import
+import { uniqueKey, getAllCol} from "./utils";
+//import { MainTable } from "../cascades/MainTable";
+//import {getCids, getInitialSelection} from "../cascades/ClusterCmpPlot";
+//import {ClusterClassesPlot} from "../ClusterClasses.js";
+//import {OutlineCards} from "../cascades/OutlineCards";
+//import { getCurrentCascade } from "../cascades/CascadeVisualizer3D";
+//import { Statistics } from "../statistics/Statistics";
+//import { Comparison } from "../Comparison/Comparison";
+//import {CascadesAndClusterCmp} from "../cascades/CascadesAndClusterCmp";
+//other components
+import Select from 'react-select';
+import Paper from '@material-ui/core/Paper';
+//import { getCids, getInitialSelection } from "ClusterCmpPlot.js";
+
+import MainTable from "./Maintable.js"
+//import {ClusterClassesTrends} from "../ClusterClassesTrends.js";
+
+const getCids = (row) => [];
+const getInitialSelection = (row) => '';
+
+const styles = theme => ({
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+  },
+  secondaryHeading: {
+    fontSize: theme.typography.pxToRem(15),
+    color: theme.palette.text.secondary,
+  },
+  icon: {
+    verticalAlign: 'bottom',
+    height: 20,
+    width: 20,
+  },
+  details: {
+    alignItems: 'center',
+  },
+  column: {
+    flexBasis: '33.33%',
+  },
+  helper: {
+    borderLeft: `2px solid ${theme.palette.divider}`,
+    padding: `${theme.spacing(1)}px ${theme.spacing(2)}px`,
+    //padding: `${theme.spacing.unit}px ${theme.spacing.unit * 2}px`,
+  },
+});
+
+export class DashboardSimple extends React.Component {
+  constructor(props) {
+      super(props);
+      this.data = this.props.data;
+      this.allCols = getAllCol();
+      const initialPick = this.data.length == 0 ? -1 : 0;
+      const initialRow = (initialPick >= 0) ? this.data[initialPick] : {};
+      this.state = {
+          curRows: this.data,
+          compareRows: new Set(),
+          except: new Set(),
+          look: "",
+          mobileOpen: false,
+          lookrow: initialRow,
+          cidCmp: getInitialSelection(initialRow),
+          allCids: getCids(initialRow),
+          showCol: this.allCols.filter(x => x['isShow'])
+      };
+  }
+
+  shortName = (row) => {
+    let name = row.id;
+    for (const x of this.state.showCol) {
+      if (x.type != 'input') continue;
+      name += "-" + x.parseFn(x.accessor(row));
+    }
+    return name;
+  }
+
+  handleShowCols = showCol => {
+   this.setState(
+      { showCol }
+    );
+  };
+
+  handleClusterCmp(cid) {
+    cid = '' + cid;
+    if (!this.state.lookrow.features.hasOwnProperty(cid)) return;
+    this.setState ({
+      cidCmp : cid
+    });
+  }
+
+  handleToggleDrawer = () => {
+    this.setState({ mobileOpen: !this.state.mobileOpen });
+  };
+
+  handleHideDrawer = () => {
+    if (this.state.mobileOpen === false) return;
+    this.setState({ mobileOpen: false });
+  };
+
+  handleShowDrawer = () => {
+    if (this.state.mobileOpen === true) return;
+    this.setState({ mobileOpen: true });
+  };
+
+  setRows(curRows) {
+    this.setState({
+      curRows,
+    });
+  }
+
+  exceptCurRowHandler(row) {
+    const isExcepted = this.state.except.has(uniqueKey(row));
+    let except = new Set(this.state.except);
+    if (isExcepted) {
+      except.delete(uniqueKey(row));
+    } else {
+      except.add(uniqueKey(row));
+    }
+    this.setState({
+      except
+    });
+  }
+
+  lookCurRowHandler(row) {
+    const isLooking = (this.state.look === row.name)
+    if (isLooking) return;
+    const compareRows = new Set(this.state.compareRows);
+    compareRows.add(uniqueKey(row));
+   this.setState({
+      look: uniqueKey(row),
+      lookrow: row,
+      compareRows,
+      cidCmp: getInitialSelection(row),
+      allCids: getCids(row)
+    });
+  }
+    
+    render() {
+    let classes = this.props.classes;
+    let open = false;
+    console.log(this.data);
+    return (
+      <div className="main-panel">
+    <AppBar>
+            <Toolbar disableGutters={!open}>
+             <Typography variant="title" color="inherit" noWrap>
+                <a id="logoTitle" href="https://github.com/haptork/csaransh">CSaransh</a>
+              </Typography>
+  
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                className={classNames(classes.menuButton, open && classes.hide)}
+              >
+              </IconButton>
+
+           </Toolbar>
+    </AppBar>
+
+        <Grid id="mainTableC" /*justify="flex-end"*/ container> 
+          <ClickAwayListener onClickAway={this.handleHideDrawer}>
+          <GridItem id="mainTable" xs={12}>
+<Accordion id="mainTablePanel" expanded={this.state.mobileOpen} onChange={this.handleToggleDrawer}>
+          <AccordionSummary /*onMouseEnter={this.handleShowDrawer}*/ expandIcon={<ExpandMoreIcon />}>
+            <Typography className={classes.heading}>Cascades List - {this.state.curRows.length} cascades {(this.state.curRows.length < this.data.length) ? " filtered out of total " + this.data.length : " - Filter, View, Plot Using Action Buttons"} 
+           </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <div style={{display:"block", width:"100%"}}>
+              <Select
+                value={this.state.showCol}
+                closeOnSelect={false}
+                multi
+                options={this.allCols}
+                onChange={this.handleShowCols}
+              />
+            </div>
+          </AccordionDetails>
+       </Accordion>
+          </GridItem>
+        </ClickAwayListener>
+          </Grid>
+          </div>
+        );
+    }
+}
+export default withStyles(styles)(withStyles(dashboardStyle)(DashboardSimple));
+/*
+                <MainTable
+                  data={this.data}
+                  except={this.state.except}
+                  look={this.state.look}
+                  setRows={(rows) => this.setRows(rows)}
+                  onLookCur={o => this.lookCurRowHandler(o)}
+                  onExceptCur={o => this.exceptCurRowHandler(o)}
+                  showCol={this.state.showCol}
+                />
+
+*/
---
+++ progress.md
@@ -0,0 +1,136 @@
+Main Branches to work:
+
+- Optimization.
+- Database.
+- Gui.
+- FCC.
+- Alloy.
+
+10th Aug:
+
+WIP:
+
+- 11pm-1pm: O: ~~plotly custom build.~~
+  - https://github.com/plotly/plotly.js/blob/master/CUSTOM_BUNDLE.md
+  - npm run custom-bundle -- --out csaransh --transforms none --traces scatter,bar,box,scatter3d,mesh3d,histogram2dcontour,heatmap
+
+- 1pm - 3pm: D: ~~Selection of DB. .~~
+  - sqlite seems okay for the following reasons.
+    - db size is less than a GB.
+    - Most of the query fields can be saved as values in the database.
+    - coordinates can be stored as text or JSON.
+    - JSON1 plugin supports json format.
+    - Need latest version of sqlite.
+    - Installing latest version on older ubuntu is difficult. Compiling for source might be a hassle as it is failing on my system too. Wasted an hour on this.
+
+- 3pm - D: Installed knex and created basic database configurations.
+
+TODO:
+
+- Create migrations for cascades table.
+- Create api to fetch records one page at a time.
+- Create api to fetch all details of a specific record.
+
+---
+
+11th Aug:
+
+WIP:
+
+- Added migrations and seeding of dev data for the table.
+  - Sought help from https://blog.bitsrc.io/seeding-your-database-with-thousands-of-users-using-knex-js-and-faker-js-6009a2e5ffbf
+  - commands to remember:
+    - ./node_modules/knex/bin/cli.js migrate:latest --env development
+    - ./node_modules/knex/bin/cli.js migrate:down --env development
+    - ./node_modules/knex/bin/cli.js seed:run --env development
+
+TODO:
+
+- ~~Create migrations for cascades table.~~
+- Create api to fetch records one page at a time.
+- Create api to fetch all details of a specific record.
+
+
+13th Aug:
+
+WIP:
+
+- ~~Api for inserting a new record.~~
+  - TODO: Testing later.
+- ~~Api for fetching all records.~~
+- ~~Api for fetching all details of a specific record.~~
+
+TODO:
+
+- Fetch records with pagination and filtering.
+- ~~Create api to fetch all details of a specific record.~~
+- ~~Change the structure of the app such that client code with react is on a sub-directory ./client.~~
+- ~~Install nodemon etc. on the server.~~
+
+14th Aug:
+
+WIP:
+
+- ~~restructured app with client react app as a sub directory. Found pointers at: ~~
+  - https://daveceddia.com/create-react-app-express-backend/#commento-login-box-container
+  - https://github.com/joshterrill/simple-express-server
+  - ~~Installed nodemon etc. as a result.~~
+  - ~~ pagination and sorting api. ~~
+
+TODO:
+
+  - Filtering api.
+
+15th Aug:
+
+WIP:
+
+- ~~Filters in all cascade api.~~
+  - curl -X GET 'http://localhost:3001/cascades?filter[id]=1&filter[ndefects][]=5&filter[ndefects][]=8'
+  - curl -X GET 'http://localhost:3001/cascades?limit=2&offset=2&sort=ndefects&filter[id]=1'
+
+- QRC for knex: https://devhints.io/knex
+
+---------------
+
+With this We're done with basic functional DB and its API.
+
+---------------
+
+TODO:
+
+- React table with ajax that uses the API exposed.
+
+16th Aug:
+
+WIP:
+
+- React table with ajax that uses the API exposed.
+  - New react table version (7) uses hooks introduced in react 17.
+  - ~~Built a minimal react table 7.~~
+  - ~~Read about hooks.~~
+  - The conclusion is that hooks are not worth the time. I'll continue with react table 6.
+- Imported material dashboard in the project.
+  - Weird issues when importing from node-modules so copied to src directory and changed a few import addresses to relative addressing.
+
+TODO:
+- ~~Import the Maintable from previous project cleanly.~~
+
+17th Aug:
+
+WIP:
+- ~~Imported the Maintable from previous project cleanly.~~
+  - Used the url query string to load the initial data that is passed to the table after the
+initial load the further filtering, sorting etc. is to be handled on client side.
+
+TODO:
+- Add header and table css.
+- Optimize the possible duplication of data.
+- Add more columns to server side such as density regions etc.
+- Add some cascade plots.
+- Experiment with table always open etc.
+- Implement statistics on client side / on server side by passing the filter string somehow.
+- Experiment with new cascade plots. Plotly save as svg.
+
+
+
