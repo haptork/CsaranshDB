bd201d30dfd3186f0e30bb450a47773450c17746
haptork <hap_tork@hotmail.com>
2021-09-02 15:21:40 +0530

    fixed clusters db

--- src/db/seeds/dev/addClusters.js
+++ src/db/seeds/dev/addClusters.js
@@ -1,35 +1,142 @@
 const devData = require("./cascades-dev.json");
 
-exports.seed = function(knex) {
+const getClusterCoord = (row, cid) => {
+  let c = [[],[],[]];
+  if (cid.length == 0 || row == undefined || !row.eigen_features.hasOwnProperty(cid)) return c;
+  for (const x of row.eigen_features[cid].coords) {
+    c[0].push(x[0]);
+    c[1].push(x[1]);
+    c[2].push(x[2]);
+  }
+  return c;
+};
+
+const getClusterLineCoord = (row, cid) => {
+  let lines = [];
+  let linesT = [];
+  let pointsI = [[], [], [], []];
+  let pointsV = [[], [], [], []];
+  cid = "" + cid;
+  if (cid.length == 0) cid = undefined;//getInitialSelection(row); // TODO
+  //console.log(row.features[cid]['lines'])
+  if (cid) {
+    //let iCount = 0
+    for (const x of row.features[cid]['lines']['linesT']) {
+      //console.log(cid);
+      let c = [[],[],[],[], [-1.0, -1.0]];
+      c[4] = x['orient']
+      for (const y of x['main']) {
+        //console.log(y);
+        //console.log(row.eigen_features[cid]['coords']);
+        // const curCoord = row.eigen_features[cid]['coords'][y]
+        const curCoord = row.coords[row.clusters[cid][y]]
+        c[0].push(curCoord[0]);
+        c[1].push(curCoord[1]);
+        c[2].push(curCoord[2]);
+        //c[3].push(y);
+        c[3].push(''+x['orient']);
+      }
+      /*
+      if (iCount < row.features[cid]['lines']['cLinesT'].length)
+        c[4] = row.features[cid]['lines']['cLinesT'][iCount++];
+        */
+      linesT.push(c);
+    }
+    for (const x of row.features[cid]['lines']['lines']) {
+      let c = [[],[],[],[], [-1.0, -1.0]];
+      let c2 = [[],[],[],[], [-1.0, -1.0]];
+      c[4] = x['orient']
+      c2[4] = x['orient']
+      for (const y of x['main']) {
+        //console.log(y);
+        //console.log(row.eigen_features[cid]['coords']);
+        // const curCoord = row.eigen_features[cid]['coords'][y]
+        const curCoord = row.coords[row.clusters[cid][y]]
+        c[0].push(curCoord[0]);
+        c[1].push(curCoord[1]);
+        c[2].push(curCoord[2]);
+        //c[3].push(y);
+        c[3].push(''+x['orient']);
+      }
+      for (const y of x['sub']) {
+        //console.log(y);
+        //console.log(row.eigen_features[cid]['coords']]);
+        // const curCoord = row.eigen_features[cid]['coords'][y]
+        const curCoord = row.coords[row.clusters[cid][y]]
+        c2[0].push(curCoord[0]);
+        c2[1].push(curCoord[1]);
+        c2[2].push(curCoord[2]);
+        //c2[3].push(y);
+        c2[3].push(''+x['orient']);
+      }
+      lines.push({main:c, sub:c2});
+    }
+    for (const x of row.features[cid]['lines']['pointsI']) {
+      // const curCoord = row.eigen_features[cid]['coords'][x]
+      const curCoord = row.coords[row.clusters[cid][x]]
+      pointsI[0].push(curCoord[0]);
+      pointsI[1].push(curCoord[1]);
+      pointsI[2].push(curCoord[2]);
+      pointsI[3].push(x);
+    }
+    for (const x of row.features[cid]['lines']['pointsV']) {
+      // const curCoord = row.eigen_features[cid]['coords'][x]
+      const curCoord = row.coords[row.clusters[cid][x]]
+      pointsV[0].push(curCoord[0]);
+      pointsV[1].push(curCoord[1]);
+      pointsV[2].push(curCoord[2]);
+      pointsV[3].push(x);
+    }
+  }
+  return {lines, linesT, pointsI, pointsV};
+};
+
+const getCoordType = (row, cid) => {
+  cid = "" + cid;
+  if (cid.length == 0 || row == undefined || !row.eigen_features.hasOwnProperty(cid)) return -1;
+  if (row.hasOwnProperty('features') &&
+      row.features.hasOwnProperty(cid) &&
+      row.features[cid].hasOwnProperty('lines')) return 1;
+  return 0;
+};
+
+const getComparisonPairs = (cascade, clusterId) => {
+  let pairs = {};
+  for (let criterion in cascade.clust_cmp[clusterId]) {
+    pairs[''+cascade.id+","+clusterId] = [cascade.id, clusterId];
+  }
+  for (let criterion in cascade.clust_cmp_size[clusterId]) {
+    pairs[''+cascade.id+","+clusterId] = [cascade.id, clusterId];
+  }
+  return pairs;
+};
+
+exports.seed = async function(knex) {
   // Deletes ALL existing entries
   const data = devData.data;
   let tableRows = [];
   for (let cascade of data) {
     for (let clusterId in cascade.clust_cmp) {
-      let pairs = {};
-      for (let criterion in cascade.clust_cmp[clusterId]) {
-        pairs[''+cascade.id+","+clusterId] = [cascade.id], clusterId];
-      }
-      for (let criterion in cascade.clust_cmp_size[clusterId]) {
-        pairs[''+cascade.id+","+clusterId] = [cascade.id, clusterId];
-      }
-      let coords = {};
-      for (let x in pairs) {
-        coords[pairs[x]] = getClusterCoords(data, pairs[x]);
-      }
+      const pairs = getComparisonPairs(cascade, clusterId);
+      const coordType = getCoordType(cascade, clusterId);
+      const coords = (coordType == 1) ?
+                getClusterLineCoord(cascade, clusterId):
+                getClusterCoord(cascade, clusterId);
+      const size = cascade.clusterSizes[clusterId];
+      const morphology = (size < 0) ? 'v' : cascade.clusterClasses.savi[clusterId];
       tableRows.push({
         'cascadeid':cascade.id,
-        'clusterName': clusterId,
-        'morphology': cascade.clusterClasses[clusterId],
-        'size': cascade.id.clusterSizes[clusterId],
-        'coords': getClusterCoords(data[cascade.id-1], clusterId),
-        'cmp': cascade.clust_cmp[clusterId],
-        'cmpsize': cascade.clust_cmp_size[clusterId],
-        'cmpcoords': coords
+        'name': clusterId,
+        'size': size,
+        'morphology': morphology,
+        'coordtype': coordType,
+        'coords': JSON.stringify(coords),
+        'cmp': JSON.stringify(cascade.clust_cmp[clusterId]),
+        'cmpsize': JSON.stringify(cascade.clust_cmp_size[clusterId]),
+        'cmppairs': JSON.stringify(pairs)
       });
     }
   }
   await knex('clusters').del();
-
-  return knex('clusters').insert();
+  return knex('clusters').insert(tableRows);
 };
